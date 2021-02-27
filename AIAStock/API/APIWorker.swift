632fcc68701c3a6bbcfec1da1a6c5574aec59a6e
Zharif Hadi <mzmhadi@gmail.com>
2021-02-27 18:54:31 +0800

    Implemented api worker

--- AIAStock/API/APIWorker.swift
+++ AIAStock/API/APIWorker.swift
@@ -7,13 +7,47 @@
 
 import Foundation
 
-class APIWorker {
+public enum ErrorType: Error {
+    case undefined
+}
+
+public enum Result<T> {
+    case success(T)
+    case failure(Error)
+}
+
+public class APIWorker {
     
     // MARK: - Properties
     
-    init() {}
+    public init() {}
     
     // MARK: - Methods
     
-    
+    public static func request(urlEndpoint: String, onResult: @escaping (Result<Any>) -> Void) {
+        guard let url = URL(string: urlEndpoint) else { return }
+
+        var dataTask: URLSessionDataTask?
+        let defaultSession = URLSession(configuration: .default)
+        
+        dataTask?.cancel()
+        
+        dataTask = defaultSession.dataTask(with: url, completionHandler: { (data, response, error) in
+
+            if let error = error {
+                onResult(.failure(error))
+            } else if
+                let data = data,
+                let response = response as? HTTPURLResponse,
+                response.statusCode == 200 {
+                DispatchQueue.main.async {
+                    onResult(.success(data))
+                }
+            } else {
+                onResult(.success(ErrorType.undefined))
+            }
+        })
+        
+        dataTask?.resume()
+    }
 }
